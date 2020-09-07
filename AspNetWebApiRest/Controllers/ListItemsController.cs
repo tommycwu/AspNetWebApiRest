6ddc511eb86078d9673d6729244c7dfee27181cc
tommycwu <tommycwu@yahoo.com>
2020-09-07 14:38:50 -0700

    Initial commit

---
+++ AspNetWebApiRest/Controllers/ListItemsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AspNetWebApiRest.Models;
+
+namespace AspNetWebApiRest.Controllers
+{
+    [Authorize]
+    public class ListItemsController : ApiController
+    {
+        private static List<CustomListItem> _listItems { get; set; } = new List<CustomListItem>();
+
+        //GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "1", "2", "3" };
+        }
+
+        public string Get(int id)
+        {
+            string s;
+            switch (id)
+            {
+                case 1:
+                    s = "1";
+                    break;
+                case 2:
+                    s = "2";
+                    break;
+                case 3:
+                    s = "3";
+                    break;
+                case 4:
+                    s = "4";
+                    break;
+                default:
+                    s = "0";
+                    break;
+            }
+            return s;
+        }
+
+        //POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        //PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        //DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AspNetWebApiRest/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Jwt;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace AspNetWebApiRest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //var authority = "https://{yourOktaDomain}/oauth2/default";
+            var oktaAudience = ConfigurationManager.AppSettings["OktaAudience"];
+            var oktaAuthority = ConfigurationManager.AppSettings["OktaAuthority"];
+
+            var configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(
+               oktaAuthority + "/.well-known/openid-configuration",
+               new OpenIdConnectConfigurationRetriever(),
+               new HttpDocumentRetriever());
+            var discoveryDocument = Task.Run(() => configurationManager.GetConfigurationAsync()).GetAwaiter().GetResult();
+
+            app.UseJwtBearerAuthentication(
+               new JwtBearerAuthenticationOptions
+               {
+                   AuthenticationMode = AuthenticationMode.Active,
+                   TokenValidationParameters = new TokenValidationParameters()
+                   {
+                       ValidAudience = oktaAudience,
+                       ValidIssuer = oktaAuthority,
+                       IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) =>
+                       {
+                           return discoveryDocument.SigningKeys;
+                       }
+                   }
+               });
+
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+
+            app.UseWebApi(config);
+        }
+    }
+}
